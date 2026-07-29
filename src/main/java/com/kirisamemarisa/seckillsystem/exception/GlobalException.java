c36e20c60aa923d7e380a4335d9ab967806d0346
KirisameMarisa0602 <3938314729@qq.com>
2026-07-29 23:23:14 +0800

    doc: 完成基于Redis的接口限流与全局异常处理测试

--- src/main/java/com/kirisamemarisa/seckillsystem/exception/GlobalException.java
+++ src/main/java/com/kirisamemarisa/seckillsystem/exception/GlobalException.java
@@ -1,25 +1,15 @@
 package com.kirisamemarisa.seckillsystem.exception;
 
-import com.kirisamemarisa.seckillsystem.vo.RespBean;
 import com.kirisamemarisa.seckillsystem.vo.RespBeanEnum;
-import org.springframework.validation.BindException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
-@RestControllerAdvice
-public class GlobalExceptionHandler {
-    @ExceptionHandler(Exception.class)
-    public RespBean ExceptionHandler(Exception e) {
-        if (e instanceof GlobalException) {
-            GlobalException ex = (GlobalException) e;
-            return RespBean.error(ex.getRespBeanEnum());
-        }
-        else if (e instanceof BindException) {
-            BindException ex = (BindException) e;
-            RespBean respBean = RespBean.error(RespBeanEnum.BIND_ERROR);
-            respBean.setMessage("参数校验异常：" + ex.getBindingResult().getAllErrors().get(0).getDefaultMessage());
-            return respBean;
-        }
-        return RespBean.error(RespBeanEnum.ERROR);
-    }
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class GlobalException extends RuntimeException {
+    private RespBeanEnum respBeanEnum;
 }
