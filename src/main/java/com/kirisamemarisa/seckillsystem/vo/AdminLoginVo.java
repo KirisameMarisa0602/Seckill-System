ab340e6666d2edbae7dd86e904c6b1a7d271cd51
KirisameMarisa0602 <3938314729@qq.com>
2026-08-05 19:16:36 +0800

    fix:管理员登陆时账号密码不为空判断逻辑改为NotBlank，防止全空格或者空字符串现象；doc:完成了商品上架和管理员登陆相关测试截图

--- src/main/java/com/kirisamemarisa/seckillsystem/vo/AdminLoginVo.java
+++ src/main/java/com/kirisamemarisa/seckillsystem/vo/AdminLoginVo.java
@@ -1,13 +1,14 @@
 package com.kirisamemarisa.seckillsystem.vo;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class AdminLoginVo {
-    @NotNull(message = "管理员账号不能为空")
+    @NotBlank(message = "管理员账号不能为空")
     private String username;
 
-    @NotNull(message = "管理员密码不能为空")
+    @NotBlank(message = "管理员密码不能为空")
     private String password;
 }
